d4f68f0ce031d7a5912efb23637a450be5afc8b9
Sereinfy <122258839+Sereinfy@users.noreply.github.com>
2025-07-13 15:25:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
-#
+#============================================================
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#============================================================
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 默认网关 ip 地址修改
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/luci2/bin/config_generate
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+#sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+
+## golang 为 1.24.x
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+
+rm -rf feeds/packages/net/mosdns
+rm -rf package/feeds/packages/mosdns
+
+rm -rf package/feeds/luci/luci-app-mosdns
+rm -rf feeds/luci/applications/luci-app-mosdns
+
+rm -rf feeds/packages/net/v2ray-geodata
+rm -rf package/feeds/packages/v2ray-geodata
+
+rm -rf package/feeds/luci/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-openclash
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/Sereinfy/v2ray-geodata package/v2ray-geodata
+git clone --depth=1 -b dev https://github.com/vernesong/OpenClash package/luci-app-openclash
+
+git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-argon/*
+cp -af package/luci-theme-argon/*  feeds/luci/themes/luci-theme-argon/
