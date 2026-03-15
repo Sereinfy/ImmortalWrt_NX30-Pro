072adbe27190ab3a7b2a36931663cd4ba5716f6c
Sereinfy <122258839+Sereinfy@users.noreply.github.com>
2026-03-15 17:21:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-sed -i '1i src-git 217heidai https://github.com/217heidai/OpenWrt-Packages' feeds.conf.default
+#sed -i '1i src-git 217heidai https://github.com/217heidai/OpenWrt-Packages' feeds.conf.default
